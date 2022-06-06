220145373ed5197351f08bc279ffd8c19c5b1de2
quoctrang.le <quoctrang.le@nfq.asia>
2022-06-06 14:52:24 +0700

    implement debounce in call api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form, Input } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { SearchOutlined } from "@ant-design/icons";
+import axios from "axios";
+const App = () => {
+  const [dataFeatch, setDataFeatch] = useState();
+  const [dataSearch, setDataSearch] = useState();
 
-function App() {
+  let timer = useRef();
+  const handleSearch = (e) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      setDataSearch(e.target.value);
+    }, 1000); 
+  };
+
+  useEffect(() => {
+    const featchAllData = async () => {
+      const { data } = await axios.get(
+        `https://hn.algolia.com/api/v1/search?query=${dataSearch}`
+      );
+      data && setDataFeatch(data.hits);
+    };
+    featchAllData();
+  }, [dataSearch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "5rem 30rem" }}>
+      <Form>
+        <Input onChange={(e) => handleSearch(e)} prefix={<SearchOutlined />} />
+      </Form>
+      <ul>
+        {dataFeatch &&
+          dataFeatch.map((item, id) => <li key={id}>{item.title}</li>)}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
